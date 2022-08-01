035c1afa015b016441166b3d2aef4a90a9b5d733
green <dengqing@users.noreply.github.com>
2022-08-01 18:36:09 +0800

    Add files via upload

---
+++ Debian Benchmark.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+dpkg -l | grep golang && sudo apt upgrade golang
+
+cat >> /etc/sysctl.conf << EOF
+#Ensure source routed packets are not accepted
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv6.conf.all.accept_source_route = 0
+net.ipv6.conf.default.accept_source_route = 0
+# Ensure core dumps are restricted
+fs.suid_dumpable = 0
+EOF
+
+# Ensure rsync service is not enabled
+systemctl --now disable rsync
+
+#  Ensure users' dot files are not group or world writable
+
+
+# Ensure SNMP Server is not enabled
+ systemctl disable snmpd
+
+ # Ensure HTTP server is not enabled
+  systemctl --now disable apache2
+
+# Ensure Avahi Server is not enabled
+systemctl --now disable avahi-daemon
+
+# Ensure users own their home directories
+
+cat >> /etc/audit/audit.rules << EOF
+# Ensure changes to system administration scope (sudoers) is collected
+-w /etc/sudoers -p wa -k scope
+-w /etc/sudoers.d/ -p wa -k scope
+
+# Ensure kernel module loading and unloading is collected
+-w /sbin/insmod -p x -k modules
+-w /sbin/rmmod -p x -k modules
+-w /sbin/modprobe -p x -k modules
+-a always,exit -F arch=b64 -S init_module -S delete_module -k modules
+
+# Ensure the audit configuration is immutable
+-e 2
+
+#  Ensure kernel module loading and unloading is collected
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+
+# Ensure successful file system mounts are collected
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+EOF
+
+cat >> /etc/audit/rules.d/audit.rules << EOF
+# Ensure changes to system administration scope (sudoers) is collected
+-w /etc/sudoers -p wa -k scope
+-w /etc/sudoers.d/ -p wa -k scope
+
+# Ensure kernel module loading and unloading is collected
+-w /sbin/insmod -p x -k modules
+-w /sbin/rmmod -p x -k modules
+-w /sbin/modprobe -p x -k modules
+-a always,exit -F arch=b64 -S init_module -S delete_module -k modules
+
+# Ensure the audit configuration is immutable
+-e 2
+
+#  Ensure kernel module loading and unloading is collected
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+
+# Ensure successful file system mounts are collected
+-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts
+EOF
+
+# Ensure SNMP Server is not enabled
+update-rc.d snmpd disable
+
+# Ensure at/cron is restricted to authorized users
+rm /etc/cron.deny
+rm /etc/at.deny
+touch /etc/cron.allow
+touch /etc/at.allow
+chmod og-rwx /etc/cron.allow
+chmod og-rwx /etc/at.allow
+chown root:root /etc/cron.allow
+chown root:root /etc/at.allow
+
+# install audit and auditd service is enable
+sudo apt install -y auditd && sudo systemctl enable auditd
+
+# Ensure permissions on /etc/ssh/sshd_config are configured
+sudo chmod -R 600 /etc/ssh/sshd_config
+sudo chown root:root /etc/ssh/sshd_config
